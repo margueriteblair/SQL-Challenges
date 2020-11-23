0b9f1526ee3dd1ccb0b2c6196cb02de420e70e06
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-23 16:30:12 -0500

    cast list for casablanca

--- joinOperation2.sql
+++ joinOperation2.sql
@@ -3,3 +3,6 @@
  WHERE yr=1962
 2. SELECT yr FROM movie WHERE title = 'Citizen Kane';
 3. SELECT id, title, yr FROM movie WHERE title LIKE 'Star Trek%' ORDER BY yr;
+4.SELECT id FROM actor WHERE name = 'Glenn Close'
+5.SELECT id FROM movie WHERE title = 'Casablanca';
+6.SELECT name FROM casting INNER JOIN actor ON actor.id = casting.actorid WHERE movieid=11768;
