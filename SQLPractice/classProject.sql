043a3f19b5527b686b80dbfc02c1f697221b1e44
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-24 12:38:48 -0500

    update

--- SQLPractice/classProject.sql
+++ SQLPractice/classProject.sql
@@ -13,6 +13,16 @@ lastName TEXT NOT NULL,
 created_on TIMESTAMP
 );
 
+CREATE TABLE posts (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+profileId INT(6) UNSIGNED NOT NULL UNIQUE REFERENCES profiles(id),
+title TEXT,
+content TEXT
+);
+
+
+
+
 INSERT INTO users (email, password, created_on)
 VALUES ('clifftest.com', 'tester', CURRENT_TIMESTAMP);
 INSERT INTO users (email, password, created_on)
@@ -24,3 +34,12 @@ VALUES ('gabe.com', 'poop', CURRENT_TIMESTAMP);
 
 INSERT INTO profiles (userId, firstName, lastName, created_on)
 VALUES (1, 'Cliff', 'Chon', CURRENT_TIMESTAMP);
+
+INSERT INTO profiles (userId, firstName, lastName, created_on)
+VALUES (1, 'Gabe', 'Sherman', CURRENT_TIMESTAMP);
+
+INSERT INTO profiles (userId, firstName, lastName, created_on)
+VALUES (1, 'Tom', 'Hodgkinson', CURRENT_TIMESTAMP);
+
+INSERT INTO posts (`profileId`, `title`, `content`)
+VALUES (1, 'SQL Syntax', 'Its not so bad');
