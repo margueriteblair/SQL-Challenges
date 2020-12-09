838031b5ab25778c3ae9f2fff4de7f5675b0f5d0
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-09 00:40:18 -0500

    sql self join

--- SQLPractice/selfJoin.sql
+++ SQLPractice/selfJoin.sql
@@ -11,3 +11,9 @@ SELECT id, name FROM stops INNER JOIN route ON route.stop = stops.id WHERE num =
 SELECT company, num, COUNT(*)
 FROM route WHERE stop=149 OR stop=53
 GROUP BY company, num HAVING COUNT(*) = 2
+
+5. --Execute the self join shown and observe that b.stop gives all the places you can get to from Craiglockhart, without changing routes. Change the query so that it shows the services from Craiglockhart to London Road.
+SELECT a.company, a.num, a.stop, b.stop
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+WHERE a.stop=53 AND b.stop=149;
