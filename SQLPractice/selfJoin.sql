be69eb10c28734b6920851f8611a8a90788fd330
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-08 23:36:01 -0500

    self join

--- SQLPractice/selfJoin.sql
+++ SQLPractice/selfJoin.sql
@@ -1,2 +1,5 @@
 1. --HOW MANY STOPS ARE IN THE DATABASE
 SELECT DISTINCT COUNT(id) FROM stops
+
+2. --Find the id value for the stop 'Craiglockhart'
+SELECT id FROM stops WHERE name = 'Craiglockhart';
