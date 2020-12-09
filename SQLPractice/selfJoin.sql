39d5dda6be3d587bcb1fe04a53b47e7f14f1a8be
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-09 01:59:45 -0500

    self join tofind the stops that connect

--- SQLPractice/selfJoin.sql
+++ SQLPractice/selfJoin.sql
@@ -25,3 +25,11 @@ FROM route a JOIN route b ON
   JOIN stops stopa ON (a.stop=stopa.id)
   JOIN stops stopb ON (b.stop=stopb.id)
 WHERE stopa.name='Craiglockhart' AND stopb.name='London Road'
+
+7. --Give a list of all the services which connect stops 115 and 137 ('Haymarket' and 'Leith')
+SELECT DISTINCT a.company, a.num
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+  JOIN stops stopa ON (a.stop=stopa.id)
+  JOIN stops stopb ON (b.stop=stopb.id)
+WHERE stopa.name='Haymarket' AND stopb.name = 'Leith'
