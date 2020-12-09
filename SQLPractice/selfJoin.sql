0602ff0843d5f6469da927d33645165726d32d5b
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-08 23:55:20 -0500

    inner join to cross reference data on each chart

--- SQLPractice/selfJoin.sql
+++ SQLPractice/selfJoin.sql
@@ -3,3 +3,6 @@ SELECT DISTINCT COUNT(id) FROM stops
 
 2. --Find the id value for the stop 'Craiglockhart'
 SELECT id FROM stops WHERE name = 'Craiglockhart';
+
+3. --Give the id and the name for the stops on the '4' 'LRT' service.
+SELECT id, name FROM stops INNER JOIN route ON route.stop = stops.id WHERE num = 4
