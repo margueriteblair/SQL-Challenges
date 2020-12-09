ebe449ed0996848eb9d177d1800af1f7cb9f75d7
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-09 03:47:53 -0500

    three joins

--- SQLPractice/selfJoin.sql
+++ SQLPractice/selfJoin.sql
@@ -42,3 +42,13 @@ FROM route a JOIN route b ON
   JOIN stops stopb ON (b.stop=stopb.id)
 WHERE stopa.name='Craiglockhart' AND stopb.name = 'Tollcross'
 
+9. --Give a distinct list of the stops which may be reached from 'Craiglockhart' by taking one bus, including 'Craiglockhart' itself, offered by the LRT company. Include the company and bus no. of the relevant services.
+select distinct stopb.name, a.company, a.num
+from route a 
+join route b on (a.company=b.company AND a.num=b.num)
+join stops stopa on (a.stop=stopa.id)
+join stops stopb on (b.stop=stopb.id)
+where
+ stopa.name='Craiglockhart' 
+ and a.company = 'LRT';
+
