12d6d3f97644c463290f6dbbaf0ae2b85db202d6
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-09 00:51:23 -0500

    finding all the stops between craiglockhart and london road

--- SQLPractice/selfJoin.sql
+++ SQLPractice/selfJoin.sql
@@ -17,3 +17,11 @@ SELECT a.company, a.num, a.stop, b.stop
 FROM route a JOIN route b ON
   (a.company=b.company AND a.num=b.num)
 WHERE a.stop=53 AND b.stop=149;
+
+6. --The query shown is similar to the previous one, however by joining two copies of the stops table we can refer to stops by name rather than by number. Change the query so that the services between 'Craiglockhart' and 'London Road' are shown. If you are tired of these places try 'Fairmilehead' against 'Tollcross'
+SELECT a.company, a.num, stopa.name, stopb.name
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+  JOIN stops stopa ON (a.stop=stopa.id)
+  JOIN stops stopb ON (b.stop=stopb.id)
+WHERE stopa.name='Craiglockhart' AND stopb.name='London Road'
