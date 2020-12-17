30664ed2f1b07a492e202325b72fae9678839628
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-17 13:31:21 -0500

    absolute value methods to find the manhattan distance between two lat and long points

--- SQLPractice/SQLPractice2.sql
+++ SQLPractice/SQLPractice2.sql
@@ -1,3 +1,5 @@
 1. SELECT company.company_code, company.founder, COUNT(DISTINCT lead_manager_code), COUNT(DISTINCT senior_manager_code),COUNT(DISTINCT manager_code), COUNT(DISTINCT employee_code) FROM company INNER JOIN employee ON employee.company_code = company.company_code 
 GROUP BY company.company_code, company.founder
 ORDER BY company.company_code;
+
+2. select ABS(MIN(lat_n)-MIN(long_w)) + ABS(MAX(lat_n)-MAX(long_w)) from station;
