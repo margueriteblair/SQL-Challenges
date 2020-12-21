17c84ba280815d2d220ee97557f5390b2166d85b
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-20 21:05:51 -0500

    sql practice

--- SQLPractice/SQLPractice2.sql
+++ SQLPractice/SQLPractice2.sql
@@ -5,3 +5,10 @@ ORDER BY company.company_code;
 2. select TRUNCATE(ABS(MIN(lat_n)-MAX(lat_n)) + ABS(MIN(long_w)-MAX(long_w)), 4) from station;
 
 3. select TRUNCATE(ROUND(ABS(MIN(lat_n)-MAX(lat_n)) + ABS(MIN(long_w)-MAX(long_w)),4), 4) from station;
+
+4. WITH salaries AS (
+    SELECT (months * salary) total_salary FROM Employee
+)
+select total_salary, COUNT(*) from salaries
+group by total_salary
+having total_salary = (SELECT MAX(total_salary) FROM salaries);
