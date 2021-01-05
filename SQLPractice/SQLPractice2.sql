60ce536da4a43da7f486709fd2032af33414d062
Marguerite Blair <marguerite.blair@gmail.com>
2021-01-05 15:31:55 -0500

    finding symmetric pairs

--- SQLPractice/SQLPractice2.sql
+++ SQLPractice/SQLPractice2.sql
@@ -41,3 +41,10 @@ SELECT IF(Grades.Grade < 8, NULL, Students.Name), Grades.Grade, Students.Marks F
 
 10. --Reviewing aggregates
 SELECT AVG(POPULATION) FROM CITY WHERE DISTRICT = 'CALIFORNIA';
+
+11. --finding symmetric pairs
+SELECT f1.X, f1.Y FROM Functions f1
+INNER JOIN Functions f2 ON f1.X=f2.Y AND f1.Y=f2.X
+GROUP BY f1.X, f1.Y
+HAVING COUNT(f1.X)>1 or f1.X<f1.Y
+ORDER BY f1.X;
