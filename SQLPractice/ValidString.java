ab68294d64ac92e79f145c78b89cd7e94dbb47de
Marguerite Blair <marguerite.blair@gmail.com>
2021-01-23 00:53:43 -0500

    lil test

---
+++ SQLPractice/ValidString.java
@@ -0,0 +1,52 @@
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ValidString {
+    public static void main(String[] args) {
+        System.out.println(validityTest("ABC200050A"));
+        System.out.println(validityTest("ABC209950A"));
+    }
+
+    public static int countValid(List<String> list) {
+        int valid = 0;
+        for (int i = 0; i < list.size(); i++) {
+            if (validityTest(list.get(i)) == true) {
+                valid++;
+            }
+        }
+        return valid;
+    }
+
+    public static boolean validityTest(String str) {
+        if (str.length() < 10 || str.length() > 12) {
+            return false;
+        }
+        String first = str.substring(0, 3);
+        if (first.charAt(0) == first.charAt(1) || first.charAt(1) == first.charAt(2) || first.charAt(0) == first.charAt(2)) {
+            return false;
+        }
+
+        String second = str.substring(3, 7);
+        Pattern pattern = Pattern.compile("19[0-9][0-9]|20[0-2][0-9]");
+        Matcher matcher = pattern.matcher(second);
+        if (matcher.find() == false) {
+            return false;
+        }
+
+        String third = str.substring(7, str.length()-1);
+        Pattern pattern2 = Pattern.compile("[10|20|50|100|200|500|1000]");
+        Matcher matcher2 = pattern2.matcher(third);
+        if (matcher2.find() == false) {
+            return false;
+        }
+
+        String last = str.substring(str.length()-1);
+        Pattern pattern3 = Pattern.compile("[A-Z]");
+        Matcher matcher3 = pattern3.matcher(last);
+        if (matcher3.find() == false) {
+            return false;
+        }
+        return true;
+    }
+}
